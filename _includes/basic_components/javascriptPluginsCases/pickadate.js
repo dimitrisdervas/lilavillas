43cc115df60819e909be59ef02055d30340ac36b
dimdervas <dimdervas@gmail.com>
2015-10-31 14:00:55 +0000

    tried to make dates plus 2 dates amd disable older dates

--- _includes/basic_components/javascriptPluginsCases/pickadate.js
+++ _includes/basic_components/javascriptPluginsCases/pickadate.js
@@ -1,4 +1,10 @@
-$('.datepicker').pickadate({
+$('.datepicker-checkin').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: +2 // Creates a dropdown of 15 years to control year
+  });
+
+$('.datepicker-checkout').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: +2, // Creates a dropdown of 15 years to control year
+    min: + 2
   });
