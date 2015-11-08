fb70da4151733e98e4e679a9367212928e852440
dimdervas <dimdervas@gmail.com>
2015-11-08 21:47:51 +0000

    added buttons to gallery page

--- _includes/basic_components/javascriptPluginsCases/owlSlideshow.js
+++ _includes/basic_components/javascriptPluginsCases/owlSlideshow.js
@@ -1,5 +1,24 @@
-$("#{{ component.data.wrapperDiv.theme.id }}").owlCarousel({
-                              {% for owlData in include.slideshowOwlData %}
-                              	{{ owlData[0] }} : {{ owlData[1]}} {% unless forloop.last%},{% endunless%}
-                              {% endfor %}
-                           });
+var owl = $('#{{ component.data.wrapperDiv.theme.id }}');
+owl.owlCarousel({
+   {% for owlData in include.slideshowOwlData %}
+         {{ owlData[0] }} : {{ owlData[1]}} {% unless forloop.last%},{% endunless%}
+       {% endfor %}
+    });
+
+$('.play').on('click',function(){
+    owl.trigger('autoplay.play.owl',[1000])
+})
+$('.pause').on('click',function(){
+    owl.trigger('autoplay.stop.owl')
+})
+
+// Go to the next item
+$('.next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.previous').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
