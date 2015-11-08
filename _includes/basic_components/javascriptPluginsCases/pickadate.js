980d072c19c78a1c5dd7c3f5125ef4deb0153be9
dimdervas <dimdervas@gmail.com>
2015-11-08 15:00:51 +0000

    no succes to use compress

--- _includes/basic_components/javascriptPluginsCases/pickadate.js
+++ _includes/basic_components/javascriptPluginsCases/pickadate.js
@@ -1,10 +1,10 @@
 $('.datepicker-checkin').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: +2 // Creates a dropdown of 15 years to control year
+    selectMonths: true, /* Creates a dropdown to control month */
+    selectYears: +2  /* Creates a dropdown of 15 years to control year */
   });
 
 $('.datepicker-checkout').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: +2, // Creates a dropdown of 15 years to control year
+    selectMonths: true, /* Creates a dropdown to control month */
+    selectYears: +2, /*  Creates a dropdown of 15 years to control year */
     min: + 2
   });
--- _includes/basic_components/javascriptPluginsCases/simpleformAjax.js
+++ _includes/basic_components/javascriptPluginsCases/simpleformAjax.js
@@ -1,26 +1,5 @@
-// $("#reservation-form").submit(function() {
-//             // validate form with parsley.
-//             $(this).parsley().validate();
-//             // if this form is valid
-//             if ($(this).parsley().isValid()) {
-//                 $.ajax({
-//                     dataType: 'jsonp',
-//                     url: "http://getsimpleform.com/messages/ajax?form_api_token=4e4801ec96e34437f63d843c5ca3e435",
-//                     data: $('#reservation-form').serialize() 
-//                 }).done(function() {
-//                     //callback which can be used to show a thank you message
-//                     //and reset the form
-//                     $('.success-message').css( "display", "block" );                    
-//                 });               
-//             }
-
-//             // prevent default so the form doesn't submit. We can return true and
-//             // the form will be submited or proceed with a ajax request.
-//             event.preventDefault();
-//         });
-
 window.Parsley.on('field:error', function() {
-  // This global callback will be called for any field that fails validation.
-//   console.log('Validation failed for: ', this.$element);
+  /* This global callback will be called for any field that fails validation. */
+/*   console.log('Validation failed for: ', this.$element); */
   $(this.$element).addClass('invalid')
 });
