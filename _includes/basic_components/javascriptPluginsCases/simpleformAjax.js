e30fb457ccc0877cbef91184cfed7d421a5f82a6
dimdervas <dimdervas@gmail.com>
2015-11-09 21:43:15 +0000

    removed form rates page shadow from prices, unseccesfull try to use url argument in reservation form

--- _includes/basic_components/javascriptPluginsCases/simpleformAjax.js
+++ _includes/basic_components/javascriptPluginsCases/simpleformAjax.js
@@ -3,3 +3,14 @@ window.Parsley.on('field:error', function() {
 /*   console.log('Validation failed for: ', this.$element); */
   $(this.$element).addClass('invalid')
 });
+
+function getUrlParameter(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+    var qsp = 'villa',
+        para = getUrlParameter(qsp);
+
+$('#villa').val(para);   //  assign URL param to select field
