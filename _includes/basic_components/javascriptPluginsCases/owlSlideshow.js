379c0a66a8fecaf99c3e73a03cfbbc4fe8bdfe28
dimdervas <dimdervas@gmail.com>
2015-11-15 20:55:57 +0000

    added to gallery title pagination of photos, and added addfields cases in titleCase

--- _includes/basic_components/javascriptPluginsCases/owlSlideshow.js
+++ _includes/basic_components/javascriptPluginsCases/owlSlideshow.js
@@ -15,13 +15,11 @@ $('.pause').on('click',function(){
 
 // Go to the next item
 $('.next').click(function() {
-    owl.trigger('autoplay.stop.owl');
     owl.trigger('next.owl.carousel');
 })
 // Go to the previous item
 $('.previous').click(function() {
     // With optional speed parameter
     // Parameters has to be in square bracket '[]'
-    owl.trigger('autoplay.stop.owl');
-    owl.trigger('prev.owl.carousel', [300]);
+    owl.trigger('prev.owl.carousel');
 })
