462a5d4fc57a03cae1fbd3434b7abe60fb45fceb
dimdervas <dimdervas@gmail.com>
2015-12-01 22:15:42 +0000

    added js code to keep block scrollpsy fixed as page scrolls

--- _includes/basic_components/javascriptPluginsCases/scrollpsy.js
+++ _includes/basic_components/javascriptPluginsCases/scrollpsy.js
@@ -1,2 +1,32 @@
 $('.scrollspy').scrollSpy();
-// $('.sidebar .scrollpsy-latest').pushpin({ top: $('.background-title').offset().top });
+var length = $('.sidebar-suggestions').height() - $('.scrollpsy-block').height() + $('.sidebar-suggestions').offset().top;
+
+    $(window).scroll(function () {
+
+        var scroll = $(this).scrollTop();
+        var height = $('.scrollpsy-block').height() + 'px';
+
+        if (scroll < $('.sidebar-suggestions').offset().top) {
+
+            $('.scrollpsy-block').css({
+                'position': 'relative',
+                'top': 'auto'
+            });
+
+        } else if (scroll > length) {
+
+            $('.scrollpsy-block').css({
+                'position': 'fixed',
+                'top': '0'
+            });
+
+        } else {
+
+            $('.scrollpsy-block').css({
+                'position': 'relative',
+                'top': 'auto'
+            });
+
+        }
+    });
+
